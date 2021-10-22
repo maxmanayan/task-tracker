ccf64112a88a90b14a3cc194f60cfe85d4b48950
Max Manayan <maximillian521@gmail.com>
2021-10-22 12:34:26 -0600

    object id import removed

--- models/Task.js
+++ models/Task.js
@@ -1,7 +1,20 @@
 // imports
 const mongoose = require("mongoose");
-const ObjectId = mongoose.Schema.Types.ObjectId;
 
 // schema
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  completed: {
+    type: Boolean,
+  },
+  date: {
+    type: Date,
+  },
+});
 
 // model export
