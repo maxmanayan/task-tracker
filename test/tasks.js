73471d618a29996c60c4c11f66882bc982de37bc
Max Manayan <maximillian521@gmail.com>
2021-10-28 12:07:00 -0600

    POST endpoints setup

--- test/tasks.js
+++ test/tasks.js
@@ -78,4 +78,68 @@ describe("All HTTP endpoints for tasks router", () => {
         });
     });
   });
+
+  describe("POST endpoint", () => {
+    it("Should return the task object that was posted to the new array", (done) => {
+      const sampleTask = {
+        text: "Write Tests",
+        day: "Oct 28th at 12:00 pm",
+        reminder: false,
+      };
+      chai
+        .request(server)
+        .post("/tasks/task")
+        .send(sampleTask)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.should.be.a("object");
+          res.body.should.have.property("_id");
+          res.body.should.have.property("text").eq(sampleTask.text);
+          res.body.should.have.property("day").eq(sampleTask.day);
+          res.body.should.have.property("reminder").eq(sampleTask.reminder);
+          done();
+        });
+    });
+    it("If req has invalid properties, should return a 400 err", (done) => {
+      const sampleTask = {
+        text: true,
+        day: 10.28,
+        reminder: "yes",
+      };
+      chai
+        .request(server)
+        .post("/tasks/task")
+        .send(sampleTask)
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+    it("If req is undefined, should return a 400 err", (done) => {
+      const sampleTask = undefined;
+      chai
+        .request(server)
+        .post("/tasks/task")
+        .send(sampleTask)
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+    it("If endpoint is invalid, should return a 404 err", (done) => {
+      const sampleTask = {
+        text: "Write Tests",
+        day: "Oct 28th at 12:00 pm",
+        reminder: false,
+      };
+      chai
+        .request(server)
+        .post("/tasks/taskd")
+        .send(sampleTask)
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
+  });
 });
