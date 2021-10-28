a503e2f3f331e2c461be65e11232b1aa9d797482
Max Manayan <maximillian521@gmail.com>
2021-10-28 11:23:41 -0600

    GET ALL test setup

--- test/tasks.js
+++ test/tasks.js
@@ -8,3 +8,17 @@ chai.should();
 chai.use(chaiHttp);
 
 // tests
+describe("All HTTP endpoints for tasks router", () => {
+  describe("GET ALL endpoint", () => {
+    it("Should return an array of all task objects", (done) => {
+      chai
+        .request(server)
+        .get("/tasks")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("array");
+          done();
+        });
+    });
+  });
+});
